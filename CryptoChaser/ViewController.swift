2b9805529f52f674506910e341b4676b8a29b5a0
Ernesto Sanchez Kuri <ernesto.sanchez.kuri@gmail.com>
2025-05-20 17:13:00 -0600

    Initial Commit

---
+++ CryptoChaser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoChaser
+//
+//  Created by Ernesto Sánchez Kuri on 20/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CryptoChaserTests/CryptoChaserTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoChaserTests.swift
+//  CryptoChaserTests
+//
+//  Created by Ernesto Sánchez Kuri on 20/05/25.
+//
+
+import Testing
+@testable import CryptoChaser
+
+struct CryptoChaserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
