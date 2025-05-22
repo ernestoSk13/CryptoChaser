656a4dfdfd12b31e0395fab7616f6130dd7d8ac5
Ernesto Sanchez Kuri <ernesto.sanchez.kuri@gmail.com>
2025-05-22 01:59:20 -0600

    Removing unused libraries and updating app icon

--- CryptoChaser/Presentation/MainView/Cell/SwiftUI/CurrencyCell.swift
+++ CryptoChaser/Presentation/MainView/Cell/SwiftUI/CurrencyCell.swift
@@ -7,7 +7,6 @@
 
 
 import SwiftUI
-import SDWebImage
 
 struct CurrencyCell: View {
     let viewModel: CurrencyCellViewModel
--- CryptoChaser/Presentation/Shared Views/RemoteImageView.swift
+++ CryptoChaser/Presentation/Shared Views/RemoteImageView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import SDWebImageSwiftUI
 
 struct RemoteImageView: View {
     let url: URL
